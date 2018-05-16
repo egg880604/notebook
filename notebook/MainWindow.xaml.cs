22d78ac7bae69f6864ba0557b4b43caef245bddb
LAPTOP-ATODRQ4Q\YISHAN <egg880604@gmail.com>
2018-05-16 22:42:00 +0800

    第二次修改

--- notebook/MainWindow.xaml.cs
+++ notebook/MainWindow.xaml.cs
@@ -27,19 +27,7 @@ namespace notebook
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //產生處存檔案的視窗
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-
-            //顯示視窗
-            Nullable<bool> result = dlg.ShowDialog();
-
-            //當按下儲存之後的反應
-            if(result==true)
-            {
-                //寫入檔案
-                System.IO.File.WriteAllText(dlg.FileName, Textarea.Text);
-            }
-            //存檔
+            
             
         }
 
@@ -56,6 +44,59 @@ namespace notebook
             {
                 Textarea.Text = System.IO.File.ReadAllText(dlg.FileName);
             }
+        }
+
+        private void SaveasBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //產生處存檔案的視窗
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            //顯示視窗
+            Nullable<bool> result = dlg.ShowDialog();
+
+            //當按下儲存之後的反應
+            if (result == true)
+            {
+                //寫入檔案
+                System.IO.File.WriteAllText(dlg.FileName, Textarea.Text);
+            }
+            //存檔
+        }
+
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.File.WriteAllText(@"C:\temp\aaa.txt", Textarea.Text);
+        }
+
+        private void BigBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BigBtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            Textarea.FontSize = 25;
+        }
+
+        private void MiddleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Textarea.FontSize = 20;
+        }
+
+        private void SmallBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Textarea.FontSize = 15;
+        }
+
+        private void WhiteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Textarea.Background = Brushes.White;
+        }
+
+        private void BlackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Textarea.Background = Brushes.Black;
         }
     }
 }
